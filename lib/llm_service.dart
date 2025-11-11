eb8f53b9ff17713bd77c208171d8a50d177cbc41
mysterious-man01 <joaop3dr03090@gmail.com>
2025-11-10 22:37:58 -0300

    Adiciona suporte ao Llama.cpp  e Google Gemini, uso de Isolate

--- lib/llm_service.dart
+++ lib/llm_service.dart
@@ -1,93 +1,93 @@
 import 'dart:async';
+import 'dart:io';
 
-import 'package:editfy_pdf/db_service.dart';
-import 'package:editfy_pdf/config_service.dart';
-import 'package:editfy_pdf/colections/message.dart';
 import 'package:editfy_pdf/colections/chat.dart';
 
 import 'package:pdfium_dart/pdfium_dart.dart';
 
 import 'package:langchain/langchain.dart';
 import 'package:langchain_openai/langchain_openai.dart';
+import 'package:langchain_google/langchain_google.dart';
+import 'package:langchain_llamacpp/langchain_llamacpp.dart';
 
 class LlmService {
-  final DbService _dbService = DbService();
-  final configTable = ConfigService();
-  final _streamController = StreamController();
-  late ChatOpenAI model;
+  late Map<String, dynamic> config;
+  late  BaseChatModel model;
   final List<ChatMessage> chatMessages = [];
-  late Chat chat;
+  late Chat? chat;
   final pdfium = Pdfium();
 
-  LlmService(this.chat){
-    if(configTable.config['backend'] == 'openai'){
-      model = ChatOpenAI(apiKey: configTable.config['openaikey']);
+  LlmService({required this.config, this.chat, String chatName='', String docPath=''}){
+    chat ??= Chat(chatName: chatName, docPath: docPath);
+    if(!File(chat!.docPath).existsSync()){
+      throw Exception('O caminho de docPath (${chat!.docPath}) não existe!');
     }
 
-    else if(configTable.config['backend'] == 'lan'){
-      model = ChatOpenAI(baseUrl: configTable.config['lanurl']);
+    if(config['service'] == 'openai'){
+      model = ChatOpenAI(apiKey: config['openaikey']);
     }
 
-    _streamController.addStream(_dbService.listenToMessage(chat));
+    if(config['service'] == 'gemini'){
+      model = ChatGoogleGenerativeAI(apiKey: config['geminikey']);
+    }
+
+    else if(config['service'] == 'custom'){
+      model = ChatOpenAI(baseUrl: '${config['lanurl']!}/v1');
+    }
+
+    else if(config['service'] == 'local'){
+      if(!File(config['modelpath']!).existsSync()){
+        throw Exception('O caminho de modelpath (${config['modelpath']}) não existe!');
+      }
+      
+      final options = ChatLlamaOptions(
+        model: config['modelpath'],
+        numCtx: 0
+      );
+
+      model = ChatLlamacpp(modelPath: config['modelpath'], defaultOptions: options);
+    }
 
     chatMessages.add(
-      ChatMessage.system('Você é um assistente que responde de forma direta usando o conteúdo de doocumentos.')
+      ChatMessage.system('Você é um assistente que responde de forma direta usando o conteúdo de documentos.')
     );
 
-    pdfium.openDoc(chat.docPath);
+    pdfium.openDoc(chat!.docPath);
 
     final npages = pdfium.countPages();
-    chatMessages.add(ChatMessage.system('Documento PDF: ${chat.chatName}'));
+    chatMessages.add(ChatMessage.system('Documento PDF: ${chat!.chatName}'));
 
-    for(int i=0; i < npages; i++){
-      chatMessages.add(
-        ChatMessage.system(
-          'Página $i\n${pdfium.getText(i)}'
-        )
-      );
+    if(npages > 1){
+      for(int i=0; i < npages; i++){
+        chatMessages.add(
+          ChatMessage.system(
+            'Página ${i + 1}\n${pdfium.getText(i)}'
+          )
+        );
+      }
+
+      chatMessages.add(ChatMessage.system('Fim do documento\n'));
+    } else{
+      chatMessages.addAll([
+        ChatMessage.system(pdfium.getText(0)),
+        ChatMessage.system('Fim do documento\n')
+      ]);
     }
-
-    chatMessages.add(ChatMessage.system('Fim do documento'));
   }
 
   void dispose(){
-    //_streamController.close(); erro
     model.close();
-  } 
-
-  void syncMessages() async{
-    List<Message> dbMessages = [];
-    final res = await _streamController.stream.first;
-      dbMessages = res;
-
-    for(var message in dbMessages){
-      if(message.isUser){
-        chatMessages.add(
-          ChatMessage.humanText(message.content!)
-        );
-      } else{
-        chatMessages.add(
-          ChatMessage.ai(message.content!)
-        );
-      }
-    }
   }
 
   Future<ChatResult?> sendMsgToModel(String text) async{
-    // Adicionar try catch
     try{
       chatMessages.add(ChatMessage.humanText(text));
 
       final resp = await model.invoke(PromptValue.chat(chatMessages));
-      chatMessages.add(ChatMessage.ai(resp.output.content));
-
-      //print(resp.output.content.trim());
-      _dbService.saveMessage(false, resp.output.content.trim(), chat);
 
       return resp;
     } catch(e) {
-      print('Erro => $e');
-      return null;
+      throw Exception(e);
     }
   }
 }
